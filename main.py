ec6b2bcbfa35e9e550a38314a0c99b843f029860
sharon-broes <sharon.broes@gmail.com>
2025-10-12 14:54:27 +0200

    Sorted list from a book

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from stats import get_num_words
-from stats import get_num_char
+from stats import get_num_words,get_num_char, sorted_list
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_num_char(text)
+    output = sorted_list(num_char)
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...") 
+    print(f"----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_char)
+    print(f"----------- Character Count ----------")
+    for item in output:
+        c = item["char"]
+        n = item["num"]
+        print(f"{c}: {n}")
+    #print(output)
+    print(f"============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def get_num_words(text):
     return len(words)
 
 def get_num_char(text):
-    lower_case_text = str.lower(text)
+    lower_case_text = text.lower()
     chars = {}
     for char in lower_case_text:
         if char in chars:
@@ -11,3 +11,21 @@ def get_num_char(text):
         else:
             chars[char] = 1
     return chars
+
+def sorted_list(chars):
+    output = []
+    for key in chars:
+        num = chars[key]
+        item = {}
+        item["char"] = key
+        item["num"] = num
+        #item = {"char": key, "num": num}
+        if key.isalpha():
+            output.append(item) 
+        #print(f"{key} {num}")
+        #print(item)
+    output.sort(key=sort_on, reverse=True)
+    return output
+
+def sort_on(element):
+    return element["num"]
